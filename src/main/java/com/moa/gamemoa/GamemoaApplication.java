fab094a9f9e913f7d71f2a1a9ade5190a0893383
박대화 <zlzl_321@naver.com>
2024-08-12 15:17:22 +0900

    Feat : gamemoa 프로젝트 생성

---
+++ src/main/java/com/moa/gamemoa/GamemoaApplication.java
@@ -0,0 +1,13 @@
+package com.moa.gamemoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamemoaApplication.class, args);
+    }
+
+}
